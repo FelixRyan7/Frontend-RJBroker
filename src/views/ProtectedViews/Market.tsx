3c881c97abe913bdb9e387b0c557f9c399d5cbc6
FelixRyan7 <fpasadase@uoc.edu>
2026-02-04 17:14:53 +0100

    add features to market

--- src/views/ProtectedViews/Market.tsx
+++ src/views/ProtectedViews/Market.tsx
@@ -1,46 +1,70 @@
 import { useContext, useEffect, useState } from "react";
-import api from "../../api/axios";
-import { jwtDecode } from "jwt-decode";
 import { AuthContext } from "../../context/AuthContext";
 import PersonalDataModal from "../../components/Modals/PersonalDataModal";
-import Button from "@mui/material/Button";
-import BasicButton from "../../components/Buttons/BasicButton";
-import axios from "axios";
-import apiWallet from "../../api/axiosWallet";
+import apiAsset from "../../api/axiosAsset";
+import type { DashboardAssets } from "../../@types/assets";
+import { AssetsSection } from "../../components/Assets/AssetsSection";
+import  SearchInput from "../../components/Inputs/SearchInput";
 
 
 export default function Dashboard() {
 
-  const { token, user, logout} = useContext(AuthContext);
+  const { token, user} = useContext(AuthContext);
+  const [assetsByType, setAssetsByType] = useState<DashboardAssets | null>(null);
+  
   
 
-  const testFetchWallet = async () => {
+  const fetchDashboardAssets = async () => {
     try {
-      const response = await apiWallet.get("/api/wallet/me", {
+      const response = await apiAsset.get("/api/assets/getAssetsByType", {
         headers: {
           Authorization: `Bearer ${token}`,
         }
         ,
       });
-
-      console.log("✅ Wallet response:", response.data);
+      setAssetsByType(response.data)
+      console.log("✅ asset response:", response.data);
 
     } catch (error) {
       console.error("❌ Error:", error);
     }
   };
-
+ 
+  useEffect(() => {
+  if (token && user?.hasPersonalData) {
+    fetchDashboardAssets();
+  }
+}, [token, user?.hasPersonalData]);
 
   
   return (
     <>
-
     <div className="min-h-screen">
-    {
-      user.hasPersonalData ? (<BasicButton onClick={testFetchWallet}>tienes acceso</BasicButton>
-      ): (<PersonalDataModal/>)
-    }
-
+      {
+        user.hasPersonalData ? (
+        <>
+        <div className="min-h-screen p-4">
+          <div className="">
+            <SearchInput/>
+          </div>
+        <div className="mt-24">
+          {assetsByType &&
+            Object.entries(assetsByType).map(([type, assets]) => (
+            <AssetsSection
+              key={type}
+              type={type}
+              assets={assets}
+            />
+            ))
+          }
+          </div>
+        </div>
+        </>
+        ):
+        (
+          <PersonalDataModal/>
+        )
+      }
     </div>
     </>
     
