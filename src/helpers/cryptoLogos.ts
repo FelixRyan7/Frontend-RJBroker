3d4bf8deddd3979f86d803edbd0bc7239f0165ef
FelixRyan7 <fpasadase@uoc.edu>
2026-02-04 17:19:15 +0100

    helper to show crypto logos

---
+++ src/helpers/cryptoLogos.ts
@@ -0,0 +1,9 @@
+ export const cryptoLogos: Record<string, string> = {
+  BTC: "https://assets.coingecko.com/coins/images/1/large/bitcoin.png",
+  ETH: "https://assets.coingecko.com/coins/images/279/large/ethereum.png",
+  ADA: "https://assets.coingecko.com/coins/images/975/large/cardano.png",
+  BNB: "https://assets.coingecko.com/coins/images/825/large/bnb-icon2_2x.png",
+  USDT: "https://assets.coingecko.com/coins/images/325/large/Tether.png",
+  DOT: "https://assets.coingecko.com/coins/images/12171/large/polkadot.png"
+  // añade más criptos según necesites
+};
